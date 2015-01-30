47ae29ea98d231c25eb4ebd46ed634ee6e1ceb7f
Paul Colomiets <paul@colomiets.name>
2015-01-30 18:58:30 +0200

    A prototype of py3 module

---
+++ py3/cantal.py
@@ -0,0 +1,196 @@
+import time
+import json
+import struct
+import warnings
+import os
+import abc
+import mmap
+from contextlib import contextmanager
+from itertools import group_by
+
+
+all_values = {}
+_timestr = struct.Struct('L')
+CACHE_LINE_SIZE = 64
+PAGE_SIZE = 4096
+
+
+class DuplicateValueException(RuntimeError):
+    """Raised when all parameters to value are duplicated"""
+
+
+class _Value(abc.ABCObject):
+    __slots__ = ()
+
+    @abc.abstractmethod
+    def _get_size(self):
+        pass
+
+    def _add_value(self, kwargs):
+        name = json.dump(kwargs)
+        if name in all_values:
+            raise DuplicateValueException(
+                "Counter {} is already defined".format(name))
+        all_values[name] = self
+
+
+class Counter(_Value):
+    __slots__ = ('_memoryview')
+
+    def __init__(self, **kwargs):
+        self._add_value(kwargs)
+
+    def __iadd__(self, value):
+        self._memoryview[0] += value
+
+    def _get_size(self):
+        return 8
+
+    def _get_type(self):
+        return ('L', 'u64')
+
+
+class Float(_Value):
+    __slots__ = ('_memoryview')
+
+    def __init__(self, **kwargs):
+        self._add_value(kwargs)
+
+    def incr(self, value=1):
+        self._memoryview[0] = value
+
+    __iadd__ = incr
+
+    def _get_size(self):
+        return 8
+
+    def _get_type(self):
+        return ('d', 'f64')
+
+    def set(self, value):
+        self._memoryview[0] = value
+
+    def __setitem__(self, key, value):
+        assert key == 0, "Only single value is expected"
+        self._memoryview[key] = value
+
+
+class Integer(_Value):
+    __slots__ = ('_memoryview')
+
+    def __init__(self, **kwargs):
+        self._add_value(kwargs)
+
+    def _get_size(self):
+        return 8
+
+    def _get_type(self):
+        return ('l', 'f64')
+
+    def set(self, value):
+        self._memoryview[0] = value
+
+    def __setitem__(self, key, value):
+        assert key == 0, "Only single value is expected"
+        self._memoryview[key] = value
+
+
+class State(_Value):
+    __slots__ = ('_memoryview', 'size')
+    HEADER_SIZE = _timestr.size()
+
+    def __init__(self, index, size=CACHE_LINE_SIZE-HEADER_SIZE, **kwargs):
+        sz = size + self.HEADER_SIZE
+        if sz & (sz - 1) or sz % CACHE_LINE_SIZE:
+            warnings.warning(
+                "Size of state counter should be multiple of {} or smaller"
+                "power of two sans header size ({}), perfect size is {}"
+                .format(CACHE_LINE_SIZE, self.HEADER_SIZE,
+                        CACHE_LINE_SIZE - self.HEADER_SIZE))
+        self.size = size
+        kwargs['index'] = index  # index is required
+        self._add_value(kwargs)
+
+    def _get_size(self):
+        return self.HEADER_SIZE + self.size
+
+    def _get_type(self):
+        return ('c', 'state{}'.format(self.size))
+
+    @contextmanager
+    def enter(self, value):
+        encoded = value.encode('utf-8')[:self.size]
+        chunk = _timestr.pack(int(time.time()*1000)) + encoded
+        self._memoryview[chunk] = chunk
+
+
+def start(path=None):
+    global all_values
+    values = list(all_values.items())
+    del all_values
+
+    get_size = lambda pair: pair[1]._get_size()
+    values.sort(key=get_size)
+
+    offset = 0
+    scheme = []
+    offsets = {}
+    for size, pairs in group_by(values, key=get_size):
+        if size & (size-1) == 0:  # power of two; let's optimize
+            if offset % size:
+                pad = size - offset % size
+                offset += pad
+                scheme.append('pad{}'.format(pad))
+        elif size % 8 == 0:
+            # unless value is small or it's size is crappy we must align to 8
+            if offset % 8:
+                pad = size - offset % 8
+                offset += pad
+                scheme.append('pad{}'.format(pad))
+        for name, value in pairs:
+            offsets[value] = offset
+            offset += size
+            _, typ = value._get_type()
+            scheme.append(typ + ': ' + name)
+
+    size = offset
+
+    if path is None:
+        path = os.environ.pop("CANTAL_PATH", None)
+    if path is None:
+        if 'XDG_RUNTIME_DIR' in os.environ:
+            path = '{}/cantal.{}'.format(
+                os.path.join(os.environ['XDG_RUNTIME_DIR']),
+                os.getpid())
+        else:
+            path = '/tmp/cantal.{}.{}'.format(
+                os.getuid(),
+                os.getpid())
+    tmppath = path + '.tmp'
+    metapath = path + '.meta'
+
+    if os.path.exists(metapath):
+        os.path.unlink(metapath)
+    if os.path.exists(path):
+        os.path.unlink(path)
+    if os.path.exists(tmppath):
+        os.path.unlink(tmppath)
+
+    with open(tmppath, 'wb') as f:
+        # We could use truncate, but it doesn't enlarge file in
+        # cross-platform fasion. Fortunately our data is small and usually
+        # in RAM anyway
+        f.write(b'\x00' * offset)
+        mem = memoryview(mmap.mmap(f, offset))
+
+    os.path.rename(tmppath, path)
+
+    with open(tmppath, 'wt') as f:
+        f.writelines(scheme)
+
+    os.path.rename(tmppath, metapath)
+
+    for value, offset in offsets:
+        vtype, _ = value._get_type()
+        size = value._get_size()
+        value._memoryview = mem[offset:offset+size]
