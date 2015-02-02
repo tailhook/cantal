3f89c51a766c6488642119fbcada90dcbc5b3f45
Paul Colomiets <paul@colomiets.name>
2015-02-02 15:02:45 +0200

    First working implementation for py3

--- py3/cantal.py
+++ py3/cantal.py
@@ -5,11 +5,11 @@ import warnings
 import os
 import abc
 import mmap
+import atexit
 from contextlib import contextmanager
-from itertools import group_by
+from itertools import groupby
 
 
-all_values = {}
 _timestr = struct.Struct('L')
 CACHE_LINE_SIZE = 64
 PAGE_SIZE = 4096
@@ -19,28 +19,127 @@ class DuplicateValueException(RuntimeError):
     """Raised when all parameters to value are duplicated"""
 
 
-class _Value(abc.ABCObject):
+class Collection(object):
+    """A collection of statistics parameters
+
+    It's just a singleton, which is hold in this module, but we use it
+    as a regular class for unittests
+    """
+
+    def __init__(self):
+        self._all_values = {}
+
+    def add(self, dimensions, value):
+        name = json.dumps(dimensions)
+        if name in self._all_values:
+            raise DuplicateValueException(
+                "Counter {} is already defined".format(name))
+        self._all_values[name] = value
+
+    def start(self, path):
+        values = list(self._all_values.items())
+        del self._all_values
+
+        get_size = lambda pair: pair[1]._get_size()
+        counter_order = lambda pair: (pair[1]._get_size(), pair[0])
+        values.sort(key=counter_order)
+
+        offset = 0
+        scheme = []
+        offsets = {}
+        for size, pairs in groupby(values, key=get_size):
+            if size & (size-1) == 0:  # power of two; let's optimize
+                if offset % size:
+                    pad = size - offset % size
+                    offset += pad
+                    scheme.append('pad {}'.format(pad))
+            elif size % 8 == 0:
+                # unless value is small or
+                # it's size is crappy we must align to 8
+                if offset % 8:
+                    pad = size - offset % 8
+                    offset += pad
+                    scheme.append('pad {}'.format(pad))
+            for name, value in pairs:
+                offsets[value] = offset
+                offset += size
+                _, typ = value._get_type()
+                scheme.append(typ + ': ' + name)
+
+        size = offset
+
+        tmppath = path + '.tmp'
+        metapath = path + '.meta'
+
+        if os.path.exists(metapath):
+            os.path.unlink(metapath)
+        if os.path.exists(path):
+            os.path.unlink(path)
+        if os.path.exists(tmppath):
+            os.path.unlink(tmppath)
+
+        with open(tmppath, 'w+b') as f:
+            # We could use truncate, but it doesn't enlarge file in
+            # cross-platform fasion. Fortunately our data is small and usually
+            # in RAM anyway
+            f.write(b'\x00' * offset)
+            f.flush()
+            mem = memoryview(mmap.mmap(f.fileno(), offset))
+
+        os.rename(tmppath, path)
+
+        with open(tmppath, 'wt') as f:
+            f.write('\n'.join(scheme))
+
+        os.rename(tmppath, metapath)
+
+        for value, offset in offsets.items():
+            vtype, _ = value._get_type()
+            size = value._get_size()
+            value._memoryview = mem[offset:offset+size].cast(vtype)
+
+        return ActiveCollection(path)
+
+
+class ActiveCollection(object):
+
+    def __init__(self, path):
+        self._path = path
+
+    def add(self, dimensions, value):
+        raise RuntimeError(
+            "Counters can't be added after collection.start()")
+
+    def start(self, path):
+        raise RuntimeError("The start() method already called")
+
+    def close(self):
+        os.unlink(self._path + '.meta')
+        os.unlink(self._path)
+
+
+global_collection = Collection()
+
+
+class _Value(abc.ABC):
     __slots__ = ()
+
+    def __init__(self, *, collection=global_collection, **kwargs):
+        collection.add(kwargs, self)
 
     @abc.abstractmethod
     def _get_size(self):
         pass
 
-    def _add_value(self, kwargs):
-        name = json.dump(kwargs)
-        if name in all_values:
-            raise DuplicateValueException(
-                "Counter {} is already defined".format(name))
-        all_values[name] = self
-
 
 class Counter(_Value):
     __slots__ = ('_memoryview')
 
-    def __init__(self, **kwargs):
-        self._add_value(kwargs)
-
     def __iadd__(self, value):
+        self._memoryview[0] += value
+        return self
+
+    def incr(self, value=1):
         self._memoryview[0] += value
 
     def _get_size(self):
@@ -52,9 +151,6 @@ class Counter(_Value):
 
 class Float(_Value):
     __slots__ = ('_memoryview')
-
-    def __init__(self, **kwargs):
-        self._add_value(kwargs)
 
     def incr(self, value=1):
         self._memoryview[0] = value
@@ -78,9 +174,6 @@ class Float(_Value):
 class Integer(_Value):
     __slots__ = ('_memoryview')
 
-    def __init__(self, **kwargs):
-        self._add_value(kwargs)
-
     def _get_size(self):
         return 8
 
@@ -97,9 +190,10 @@ class Integer(_Value):
 
 class State(_Value):
     __slots__ = ('_memoryview', 'size')
-    HEADER_SIZE = _timestr.size()
+    HEADER_SIZE = _timestr.size
+    assert HEADER_SIZE == 8, 'We use constants in enter/exit/context'
 
-    def __init__(self, index, size=CACHE_LINE_SIZE-HEADER_SIZE, **kwargs):
+    def __init__(self, size=CACHE_LINE_SIZE-HEADER_SIZE, **kwargs):
         sz = size + self.HEADER_SIZE
         if sz & (sz - 1) or sz % CACHE_LINE_SIZE:
             warnings.warning(
@@ -108,53 +202,40 @@ class State(_Value):
                 .format(CACHE_LINE_SIZE, self.HEADER_SIZE,
                         CACHE_LINE_SIZE - self.HEADER_SIZE))
         self.size = size
-        kwargs['index'] = index  # index is required
-        self._add_value(kwargs)
+        super().__init__(**kwargs)
 
     def _get_size(self):
         return self.HEADER_SIZE + self.size
 
     def _get_type(self):
-        return ('c', 'state {}'.format(self.size))
+        return ('B', 'state {}'.format(self.size + self.HEADER_SIZE))
 
     @contextmanager
     def enter(self, value):
-        encoded = value.encode('utf-8')[:self.size]
+        encoded = value.encode('utf-8')
+        le = len(encoded)
+        tail = le - self.size
+        if tail < 0:
+            encoded += b'\x00'
+            le += 1
+        elif tail > 0:
+            encoded = encoded[:self.size]
+        le += 8
         chunk = _timestr.pack(int(time.time()*1000)) + encoded
-        self._memoryview[chunk] = chunk
+        self._memoryview[0:le] = chunk
+
+    @contextmanager
+    def context(self, value):
+        self.enter(value)
+        yield self
+        self._memoryview[0:8] = b'\x00\x00\x00\x00\x00\x00\x00\x00'
+
+    def exit(self):
+        self._memoryview[0:8] = b'\x00\x00\x00\x00\x00\x00\x00\x00'
 
 
 def start(path=None):
-    global all_values
-    values = list(all_values.items())
-    del all_values
-
-    get_size = lambda pair: pair[1]._get_size()
-    values.sort(key=get_size)
-
-    offset = 0
-    scheme = []
-    offsets = {}
-    for size, pairs in group_by(values, key=get_size):
-        if size & (size-1) == 0:  # power of two; let's optimize
-            if offset % size:
-                pad = size - offset % size
-                offset += pad
-                scheme.append('pad{}'.format(pad))
-        elif size % 8 == 0:
-            # unless value is small or it's size is crappy we must align to 8
-            if offset % 8:
-                pad = size - offset % 8
-                offset += pad
-                scheme.append('pad{}'.format(pad))
-        for name, value in pairs:
-            offsets[value] = offset
-            offset += size
-            _, typ = value._get_type()
-            scheme.append(typ + ': ' + name)
-
-    size = offset
-
+    global global_collection
     if path is None:
         path = os.environ.pop("CANTAL_PATH", None)
     if path is None:
@@ -166,31 +247,6 @@ def start(path=None):
             path = '/tmp/cantal.{}.{}'.format(
                 os.getuid(),
                 os.getpid())
-    tmppath = path + '.tmp'
-    metapath = path + '.meta'
 
-    if os.path.exists(metapath):
-        os.path.unlink(metapath)
-    if os.path.exists(path):
-        os.path.unlink(path)
-    if os.path.exists(tmppath):
-        os.path.unlink(tmppath)
-
-    with open(tmppath, 'wb') as f:
-        # We could use truncate, but it doesn't enlarge file in
-        # cross-platform fasion. Fortunately our data is small and usually
-        # in RAM anyway
-        f.write(b'\x00' * offset)
-        mem = memoryview(mmap.mmap(f, offset))
-
-    os.path.rename(tmppath, path)
-
-    with open(tmppath, 'wt') as f:
-        f.writelines(scheme)
-
-    os.path.rename(tmppath, metapath)
-
-    for value, offset in offsets:
-        vtype, _ = value._get_type()
-        size = value._get_size()
-        value._memoryview = mem[offset:offset+size]
+    global_collection = global_collection.start(path)
+    atexit.register(global_collection.cleanup)
---
+++ py3/tests/test_stats.py
@@ -0,0 +1,138 @@
+import os
+import struct
+import textwrap
+from cantal import Collection, Counter, Float, Integer, State
+from unittest import TestCase
+
+
+class TestBase(TestCase):
+
+    def setUp(self):
+        self.collection = Collection()
+
+    def start(self):
+        path = '{}/cantal-test.{}'.format(
+            os.path.join(os.environ.get('XDG_RUNTIME_DIR', '/tmp')),
+            os.getpid())
+        self._path = path
+        active = self.collection.start(path)
+        self.addCleanup(active.close)
+        del self.collection
+
+    def counter(self, **kwargs):
+        return Counter(collection=self.collection, **kwargs)
+
+    def float(self, **kwargs):
+        return Float(collection=self.collection, **kwargs)
+
+    def integer(self, **kwargs):
+        return Integer(collection=self.collection, **kwargs)
+
+    def state(self, **kwargs):
+        return State(collection=self.collection, **kwargs)
+
+    def assertRead(self, value, offset=0):
+        with open(self._path, 'rb') as file:
+            file.seek(offset, 0)
+            self.assertEqual(file.read(), value)
+
+    def assertMeta(self, value):
+        with open(self._path + '.meta', 'rt') as file:
+            self.assertEqual(file.read(), textwrap.dedent(value).strip())
+
+
+class TestValues(TestBase):
+
+    def test_counter(self):
+        cnt = self.counter(name="hello")
+        self.start()
+        cnt += 1
+        self.assertRead(struct.pack('L', 1))
+        cnt += 3
+        self.assertRead(struct.pack('L', 4))
+        cnt.incr(1234)
+        self.assertRead(struct.pack('L', 1238))
+
+    def test_float(self):
+        cnt = self.float(name="hello")
+        self.start()
+        cnt[0] = 1.5
+        self.assertRead(struct.pack('d', 1.5))
+        cnt[0] = 0.75
+        self.assertRead(struct.pack('d', 0.75))
+        cnt.set(3.75)
+        self.assertRead(struct.pack('d', 3.75))
+        cnt.set(32)
+        self.assertRead(struct.pack('d', 32))
+
+    def test_int(self):
+        cnt = self.integer(name="hello")
+        self.start()
+        cnt[0] = 3
+        self.assertRead(struct.pack('l', 3))
+        cnt[0] = -1000
+        self.assertRead(struct.pack('l', -1000))
+        cnt.set(123564)
+        self.assertRead(struct.pack('l', 123564))
+
+    def test_state(self):
+        state = self.state(name="hello", value="world")
+        self.start()
+        with state.context('job1'):
+            self.assertRead(b'job1' + b'\x00'*52, 8)
+        # we leave garbage, but assert that timestamp is gone
+        self.assertRead(b'\x00'*8 + b'job1' + b'\x00'*52)
+
+        state.enter('some_longer_job_name')
+        self.assertRead(b'some_longer_job_name' + b'\x00'*36, 8)
+        state.exit()
+
+        self.assertRead(b'\x00'*8 + b'some_longer_job_name' + b'\x00'*36)
+        with state.context('short'):
+            self.assertRead(b'short\x00onger_job_name' + b'\x00'*36, 8)
+        self.assertRead(b'\x00'*8 + b'short\x00onger_job_name' + b'\x00'*36)
+
+
+class TestScheme(TestBase):
+
+    def test_two_counters(self):
+        self.counter(name="1")
+        self.counter(name="2")
+        self.start()
+        self.assertMeta("""
+            counter u64: {"name": "1"}
+            counter u64: {"name": "2"}
+        """)
+
+    def test_counter_float(self):
+        self.counter(name="2")
+        self.float(name="1")
+        self.start()
+        self.assertMeta("""
+            level f64: {"name": "1"}
+            counter u64: {"name": "2"}
+        """)
+
+    def test_counter_state(self):
+        self.counter(name="2")
+        self.state(name="1")
+        self.start()
+        self.assertMeta("""
+            counter u64: {"name": "2"}
+            pad 56
+            state 64: {"name": "1"}
+        """)
+
+    def test_2counters_state(self):
+        self.counter(name="1")
+        self.float(name="2")
+        self.counter(name="3")
+        self.state(name="100")
+        self.start()
+        self.assertMeta("""
+            counter u64: {"name": "1"}
+            level f64: {"name": "2"}
+            counter u64: {"name": "3"}
+            pad 40
+            state 64: {"name": "100"}
+        """)
