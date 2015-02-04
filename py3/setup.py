36d24911d13e76afadc830da77462d6e06d204e5
Paul Colomiets <paul@colomiets.name>
2015-02-04 15:52:02 +0200

    Add setup.py

---
+++ py3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cantal',
+      version='0.1',
+      description='The client library for monitoring and heartbeating',
+      author='Paul Colomiets',
+      author_email='paul@colomiets.name',
+      url='http://github.com/tailhook/cantal',
+      py_modules=['cantal'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      )
