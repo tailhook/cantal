620d400a31197d5d03f297fdca3635f4e2ec3b4a
Paul Colomiets <paul@colomiets.name>
2015-02-20 13:38:31 +0200

    Change default host of agent to 127.0.0.1

--- src/agent/main.rs
+++ src/agent/main.rs
@@ -18,7 +18,7 @@ mod staticfiles;
 
 
 fn main() {
-    let mut host = "0.0.0.0".to_string();
+    let mut host = "127.0.0.1".to_string();
     let mut port = 22682u16;
     {
         let mut ap = ArgumentParser::new();
@@ -27,7 +27,7 @@ fn main() {
                 "Port for http interface");
         ap.refer(&mut host)
             .add_option(&["-h", "--host"], Box::new(Store::<String>),
-                "Host for http interface. Warning default one is 0.0.0.0");
+                "Host for http interface (default 127.0.0.1)");
         match ap.parse_args() {
             Ok(()) => {}
             Err(x) => {
