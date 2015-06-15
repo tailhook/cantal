f1039d9a077c6e20dc77e107aec8c5a2dbbb7970
Paul Colomiets <paul@colomiets.name>
2015-06-15 20:17:08 +0300

    Add CSS auto-loading

--- frontend/marafet.js
+++ frontend/marafet.js
@@ -3,9 +3,11 @@ var child = require('child_process')
 module.exports = function(content) {
     this.cacheable()
     var callback = this.async()
+    var pathname = path.relative(process.cwd(), this.resourcePath);
+    var amd_name = path.dirname(pathname) + path.basename(pathname, '.mft')
     var ch = child.exec(
-        "../marafet -f- --amd --js -"
-        + " --amd-name '" + this.resourcePath + "'"
+        "../marafet -f- --amd --auto-load-css --js -"
+        + " --amd-name '" + amd_name + "'"
         + " --block-name 'b-" + path.basename(this.resourcePath, '.mft') + "'"
         , function(error, stdout, stderr) {
         if(error) {
