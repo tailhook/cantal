e5e97386d5ca447443399b89167d372a3a43ffde
Paul Colomiets <paul@colomiets.name>
2015-07-14 19:26:07 +0300

    WIP: ioloop

--- src/agent/ioloop.rs
+++ src/agent/ioloop.rs
@@ -1,13 +1,37 @@
 use std::collections::HashMap;
 
-use mio::{Token, Timeout, EventSet};
+use mio;
+use mio::{Token, Timeout, EventSet, Sender, Evented};
 
 use super::deps::Dependencies;
 
 
-trait Consumer {
-    fn ready(self) -> Option<(EventSet, Option<u64>, Self)>;
-    fn timeout(self) -> Option<Self>;
+enum Notify {
+    DestroySocket(Box<Evented+Send>),
+}
+
+struct SocketRef<T:Evented+Send+'static>(Option<T>, Sender<Notify>);
+
+impl<T:Evented+Send+'static> Drop for SocketRef<T> {
+    fn drop(&mut self) {
+        self.1.send(Notify::DestroySocket(
+            Box::new(self.0.take().unwrap()) as Box<Evented+Send>));
+    }
+}
+
+trait Acceptor {
+    fn accept(&self, TcpStream) -> Option<Streamer>;
+}
+
+trait Streamer {
+    fn write_buf<'x>(&'x mut self) -> Option<&mut Vec<u8>>;
+    fn read_buf<'x>(&'x mut self) -> Option<&mut Vec<u8>>;
+    fn process_data(self) -> Option<Self>;
+}
+
+enum Sink {
+    Accept(Box<Acceptor>),
+    Stream(Box<Streamer>),
 }
 
 
@@ -21,4 +45,9 @@ struct Cell {
 struct Handler {
     dependencies: Dependencies,
     connections: HashMap<Token, Cell>,
+}
+
+
+impl mio::Handler for Handler {
+    fn ready(
 }
