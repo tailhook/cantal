5c1aa7c30625ead453a60a1afa2aefb74b5af2e2
Paul Colomiets <paul@colomiets.name>
2016-02-04 20:07:03 +0200

    Add default port for the config

--- src/agent/carbon/config.rs
+++ src/agent/carbon/config.rs
@@ -12,7 +12,7 @@ pub struct Config {
 pub fn validator<'x>() -> Structure<'x> {
     Structure::new()
     .member("host", Scalar::new())
-    .member("port", Scalar::new())
+    .member("port", Scalar::new().default(2003))
     .member("interval",
         Numeric::new().min(1).max(86400).default(10))
     .member("enable_cgroup_stats", Scalar::new().default(false))
