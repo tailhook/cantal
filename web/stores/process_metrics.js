d427c1106e2f9d526e33831419f3b0cdf9cb4c3d
Paul Colomiets <paul@colomiets.name>
2016-04-28 15:52:38 +0300

    /local/process_metrics: Split table into groups, add some process info

--- web/stores/process_metrics.js
+++ web/stores/process_metrics.js
@@ -4,14 +4,30 @@ import {METRICS} from '../middleware/local-query.js'
 
 export function metrics(state=null, action) {
     if(action.type == METRICS) {
-        return action.metrics
+        let map = new Map()
+        for(let tuple of action.metrics.values) {
+            let [k] = tuple;
+            k.pid = parseInt(k.pid);
+            let lst = map.get(k.pid);
+            if(lst == null) {
+                lst = []
+                map.set(k.pid, lst)
+            }
+            lst.push(tuple);
+        }
+        return map
     }
     return state;
 }
 
 export function processes(state=null, action) {
     if(action.type == DATA) {
-        return action.data;
+        let map = new Map()
+        for(let item of action.data.all) {
+            item.cmdline.replace('\u0000', ' ')
+            map.set(item.pid, item);
+        }
+        return map;
     }
     return state;
 }
