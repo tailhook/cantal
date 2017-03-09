f67add35d019cc895819eaa6031ac6dcfe848377
Paul Colomiets <paul@colomiets.name>
2017-03-09 15:19:11 +0200

    Version bumped to v0.5.2

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -58,7 +58,7 @@ module.exports = {
     plugins: [
         new webpack.NoErrorsPlugin(),
         new webpack.DefinePlugin({
-            VERSION: JSON.stringify("0.5.1"),
+            VERSION: JSON.stringify("0.5.2"),
             "process.env.NODE_ENV": JSON.stringify(process.env['NODE_ENV']),
             DEBUG: DEV,
         }),
